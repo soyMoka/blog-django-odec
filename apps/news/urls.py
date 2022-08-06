260678826a035e97a98bf6f738a0ce2645ad37b2
Matias Cejas Schmoker <d15.cejas.matias@gmail.com>
2022-08-05 23:06:10 -0300

    comienzo de creacion noticias, creacion de noticias, modificaciones css y templates

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -5,7 +5,7 @@ app_name = 'apps.news'
 
 urlpatterns = [
     path('addNews/', views.addNews.as_view()),
-    #path('listarNews/', views.mostrarNews.as_view()),
+    path('listarNews/', views.mostrarNews.as_view()),
     #path('listarNews2/', views.ListarNews()),
     #path('listarCategoria/<str:categoria>', views.ListarNewsPorCategoria, name='listarCategoria'),
     
--- apps/news/views.py
+++ apps/news/views.py
@@ -7,16 +7,26 @@ from django.views.generic.list import ListView
 from .models import News, Categorias
 # Create your views here.
 
+
+################# ||Agragar Post ############################## 
+
 class addNews(CreateView):
     model=News
     fields = ['title', 'text', 'Categorias', 'imagen']
     template_name = 'news/addNews.html'
     success_url = reverse_lazy('index')
 
+#-----------END---END---END---END---END---END---END------------
+
+################# ||Mostrar Posts #############################
+
 class mostrarNews(ListView):
     model = News
     template_name: str = 'news/listarNews.html'
-    
+
+#-----------END---END---END---END---END---END---END------------  
+
+
 def ListarNews(request):
     noticia = News
     context = {
@@ -24,3 +34,11 @@ def ListarNews(request):
     }
     return render(request, 'news/listarNews2.html', context)
 
+def listarNewsPorCaregorias(request, categoria):
+    categoria2 = Categorias.objects.filter(nombre = Categorias)
+
+    noticia = News.objects.filter(categorias = categoria2[0].id)
+    context = {
+        'noticia': noticia
+    }
+    return render(request, 'noticia/listarNews2.html', context)
